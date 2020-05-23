3b2853c1b6308a8717fabb66cd39f7ca4871e948
Alexander Lomovskiy <a@lomovskiy.com>
2020-05-23 10:04:51 +0300

    Enable Kotlin DSL in gradle scripts #2

---
+++ sample/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion = "29.0.3"
+    defaultConfig {
+        applicationId = "com.lomovskiy.android.library.imagepicker.sample"
+        minSdkVersion(16)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("androidx.core:core-ktx:1.0.2")
+}
